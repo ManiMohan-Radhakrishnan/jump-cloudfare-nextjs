5045ff23729c255f4c4e058cccb6097a92c820b1
ManiMohan R <manimohan.r@blockchainappfactory.com>
2023-06-05 12:59:13 +0530

    changes

--- components/bns/nft-section-title/index.js
+++
@@ -1,7 +0,0 @@
-// import style from "./style.module.scss";
-
-const NFTSectionTitle = ({ title }) => {
-  return <div className={"section-title"}>{title}</div>;
-};
-
-export default NFTSectionTitle;
